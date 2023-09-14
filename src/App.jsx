6bf461a8d04a509fc5e72954a56958dfb381e562
Utku Cikmaz <utkucikmaz@gmail.com>
2023-09-14 17:15:04 +0200

    character routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import Contact from "./components/Contact";
+import About from "./components/About";
 import "./App.css";
+import CharacterDetails from "./components/CharacterDetails";
 
 function App() {
     const baseURL = "https://ih-crud-api.herokuapp.com";
@@ -24,35 +28,54 @@ function App() {
         setCharacterList(newList);
     };
     const renderList = () => {
-        return (
-            <>
-                {characterList.map((element, index) => {
-                    return (
-                        <div key={index}>
-                            <br />
-                            <h2>Name: {element.name}</h2>
-                            <h3>Weapon: {element.weapon}</h3>
-                            <br />
-                            <button
-                                onClick={() => {
-                                    deleteChar(element.id);
-                                }}
-                            >
-                                Delete
-                            </button>
-                            <hr />
-                        </div>
-                    );
-                })}
-            </>
-        );
+        if (characterList === null) {
+            return <p>loading....</p>;
+        }
+
+        return characterList.map((characterObj, index) => {
+            return (
+                <div key={index} className="card">
+                    <br />
+                    <h3>{characterObj.name}</h3>
+                    <p>{characterObj.occupation}</p>
+                    <NavLink to={"/characters/" + characterObj.id}>
+                        Details
+                    </NavLink>
+                    <br />
+                    <br />
+                    <button
+                        onClick={() => {
+                            deleteChar(characterObj.id);
+                        }}
+                    >
+                        Delete
+                    </button>
+                    <br />
+                </div>
+            );
+        });
     };
     return (
         <>
-            {characterList && (
-                <h1>Number of the chracters API: {characterList.length}</h1>
-            )}
-            {characterList === null ? <p>Loading...</p> : renderList()}
+            <nav>
+                <NavLink to="/">Home</NavLink> |
+                <NavLink to="/contact">Contact</NavLink> |
+                <NavLink to="/about">About</NavLink>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={renderList()} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+                <Route
+                    path="/characters/:characterId"
+                    element={<CharacterDetails />}
+                />
+                <Route
+                    path="*"
+                    element={<p>There is no page under this Route</p>}
+                />
+            </Routes>
         </>
     );
 }
---
+++ src/components/CharacterDetails.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function CharacterDetails() {
+    const { characterId } = useParams();
+    const [characterDetail, setCharacterDetail] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get("https://ih-crud-api.herokuapp.com/characters/" + characterId)
+            .then((response) => {
+                setCharacterDetail(response.data);
+            })
+            .catch((e) => {
+                console.log("error getting characters", e);
+            });
+    }, []);
+
+    const renderCharacterDetails = () => {
+        return (
+            <>
+                <h1>{characterDetail.name}</h1>
+                <h2>{characterDetail.occupation}</h2>
+                <h2>{characterDetail.weapon}</h2>
+            </>
+        );
+    };
+    return (
+        <section>
+            {characterDetail === null ? (
+                <p>Loading</p>
+            ) : (
+                renderCharacterDetails()
+            )}
+        </section>
+    );
+}
